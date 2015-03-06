9b674e54af4b4725b711dfc48b360143fe20c4f9
Kat Sullivan <kmsullivan012@gmail.com>
2015-03-05 23:04:24 -0500

    Brick wall has every other row stacked to look more like a brick wall

--- GameController.js
+++ GameController.js
@@ -7,7 +7,11 @@ function Start () {
 	// create a group of bricks 20 x 5
 	for(var i: int=0; i < 20; i++) {
 		for(var j: int=0; j < 5; j++) {
-			Instantiate(brick, Vector3(i-9.5, j*.5+2, 0), Quaternion.identity);
+			if(j % 2 == 0) {
+				Instantiate(brick, Vector3(i-9.5, j*.5+2, 0), Quaternion.identity);
+			} else {
+				Instantiate(brick, Vector3(i-9, j*.5+2, 0), Quaternion.identity);
+			}
 		}
 	}
 	
