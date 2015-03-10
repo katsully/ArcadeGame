3c79be5272367be08ba7fae398df5c1279473d97
Kat Sullivan <kmsullivan012@gmail.com>
2015-03-09 23:36:10 -0400

    Paddles can pass through wall once they collect gem

--- Paddle2Movement.js
+++ Paddle2Movement.js
@@ -4,9 +4,10 @@ var moveLeft : KeyCode;
 var moveRight : KeyCode;
 
 var speed : float = 15;
+var canPass : boolean = false;
 
 function Update () {
-	if(Input.GetKey(moveRight) && rigidbody2D.transform.position.x < 9){
+	if(Input.GetKey(moveRight) && (rigidbody2D.transform.position.x < 9 || canPass)){
 		rigidbody2D.velocity.x = speed;
 	} else if(Input.GetKey(moveLeft) && rigidbody2D.transform.position.x > 2.25){
 		rigidbody2D.velocity.x = speed * -1;
@@ -18,5 +19,6 @@ function Update () {
 function OnCollisionEnter2D( coll : Collision2D){
 	if(coll.gameObject.tag == "Gem"){
 		Destroy(coll.gameObject);
+		canPass = true;
 	} 
 }
--- PaddleMovement.js
+++ PaddleMovement.js
@@ -4,9 +4,10 @@ var moveLeft : KeyCode;
 var moveRight : KeyCode;
 
 var speed : float = 15;
+var canPass : boolean = false;
 
 function Update () {
-	if(Input.GetKey(moveRight) && rigidbody2D.transform.position.x < -1.5){
+	if(Input.GetKey(moveRight) && (rigidbody2D.transform.position.x < -1.5 || canPass)){
 		rigidbody2D.velocity.x = speed;
 	} else if(Input.GetKey(moveLeft) && rigidbody2D.transform.position.x > -9){
 		rigidbody2D.velocity.x = speed * -1;
@@ -18,5 +19,6 @@ function Update () {
 function OnCollisionEnter2D( coll : Collision2D){
 	if(coll.gameObject.tag == "Gem"){
 		Destroy(coll.gameObject);
+		canPass = true;
 	} 
 }
