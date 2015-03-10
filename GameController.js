e62d918e8433e3dd01843faf9678026c84c269f5
Kat Sullivan <kmsullivan012@gmail.com>
2015-03-09 23:52:07 -0400

    Split brick wall

--- GameController.js
+++ GameController.js
@@ -9,6 +9,9 @@ function Start () {
 
 	// create a group of bricks 20 x 5
 	for(var i: int=0; i < 20; i++) {
+		if(i == 10 || i == 9){
+			continue;
+		}
 		for(var j: int=0; j < 5; j++) {
 			if(j % 2 == 0) {
 				Instantiate(brick, Vector3(i-9.5, j*.5+2, 0), Quaternion.identity);
